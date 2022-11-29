6c15782dde79b749e44a88713992957be5d3bc8d
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-29 16:30:24 +0900

    test(RacingcarGameTest): RacingcarGame 클래스 생성자 수정으로 테스트 코드 변경

--- src/test/java/racingcar/RacingcarGameTest.java
+++ src/test/java/racingcar/RacingcarGameTest.java
@@ -14,7 +14,7 @@ public class RacingcarGameTest {
     @DisplayName("자동차 이름 입력이 영문자가 아니면 예외가 발생한다.")
     @ValueSource(strings = {"p iv, df", "d1,33d", "ㄴ,ㄷ,a", "하하,abc"})
     void createLineUpByNotEnglish(String input) {
-        assertThatThrownBy(() -> new RacingcarGame().createLineUp(input))
+        assertThatThrownBy(() -> new RacingcarGame(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(CAR_NAME_NOT_ENGLISH);
     }
