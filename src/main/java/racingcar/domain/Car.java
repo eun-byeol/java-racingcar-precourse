010b622de25027e8de913dc5f3318e52601594c3
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-29 15:11:57 +0900

    feat(Car): 글자수가 1보다 작거나 5자를 넘는 경우 예외 처리 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,19 @@
-package racingcar;
+package racingcar.domain;
+
+import static racingcar.utils.ErrorMessage.*;
 
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateSize(name);
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private void validateSize(String name) {
+        if (name.length() < 1 || name.length() > 5) {
+            throw new IllegalArgumentException(CAR_NAME_SIZE_OUT_OF_RANGE);
+        }
+    }
 }
