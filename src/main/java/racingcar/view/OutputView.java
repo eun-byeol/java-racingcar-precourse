3dc4a2bc5643257006818965c38c622fda97c978
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-29 20:00:08 +0900

    feat(OutputView): 실행 결과 출력 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
+import racingcar.domain.CarLineUp;
+import static racingcar.utils.ViewMessages.*;
+
 public class OutputView {
+    public static void printExecutionResult() {
+        System.out.println(OUTPUT_EXECUTION_RESULT);
+    }
+    public static void printRacingResult(CarLineUp carLineUp) {
+        carLineUp.printPlayers();
+        System.out.println();
+    }
 }
